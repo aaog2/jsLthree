1948d187e2ff11128519a8e58c637ce40c27ddc8
Aung Aung Oo <aungaungoogame2@gmail.com>
2021-10-12 11:56:10 +0630

    12/10/2021

--- draganddrop/js/app.js
+++ draganddrop/js/app.js
@@ -13,13 +13,75 @@ function dragstart(){
 
     // += append
     this.className += " hold";
+
+    setTimeout(()=>{
+        this.className="invisible";
+    },0);
+
 }
+
+// console.log(emptyes);
+
+// emptyes.forEach(emptye=>{
+//     // console.log(emptye);
+//     emptye.addEventListener("dragover",dragover);
+//     emptye.addEventListener("dragenter",dragenter);
+//     emptye.addEventListener("dragleave",dragleave);
+//     emptye.addEventListener("drop",dragdrop);
+// })
+
+
+// ES6 .can looping object5-specific iterator value
+for(const emptye of emptyes){
+    // console.log(emptye);
+
+    emptye.addEventListener("dragover",dragover);
+    emptye.addEventListener("dragenter",dragenter);
+    emptye.addEventListener("dragleave",dragleave);
+    emptye.addEventListener("drop",dragdrop);
+}
+
+// =>numerable properties name of objects
+// for(const empty in emptyes){
+//     console.log(empty);
+// }
+
 
 // Drag End
 function dragend(){
-    console.log("drag end is working");
-
+    // console.log("drag end is working");
     this.className = "fill";
+}
+
+
+// drag in empty box
+
+function dragover(e){
+    // console.log("dragover");
+    e.preventDefault();
+}
+
+
+function dragenter(e){
+    // console.log("dragenter");
+    e.preventDefault();
+
+    this.className += " hovered";
+}
+
+function dragleave(e){
+    // console.log("dragleave");
+    e.preventDefault();
+
+    this.className = "empty";
+}
+
+function dragdrop(e){
+    // console.log("dragdrop");
+    // e.preventDefault(); pay lal ya ma pay lal ya
+
+    this.className = "empty";
+    this.appendChild(picdiv);
 }
 
 // 25DND
